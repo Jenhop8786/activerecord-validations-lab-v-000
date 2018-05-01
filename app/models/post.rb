6648ce69025c9ecd11e1554ced44e20c53dd5492
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-01 23:01:31 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Nonfiction) }
+  validates :category, inclusion: {in: %w(Fiction Nonfiction)}
   validate :clickbait
 end
 
