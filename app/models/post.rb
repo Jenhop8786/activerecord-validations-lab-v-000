5e9b092a1b7cc303e122e18257f8d4ff2d766446
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-01 23:02:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validate :clickbait
 end
 
-CLICKBAIT = ["Won't Believe", "Secret", "Top[0-9]", "Guess"]
+CLICKBAIT = [/Won\'t Believe/i, /Secret/i, /Top[0-9]*/i, /Guess/i]
 
   def clickbait
     if CLICKBAIT.none? {|t| t.match title}
